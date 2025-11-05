28e38f6003e339061ec450668c4fc717924422eb
Santiago Gomez <santiago.gomezl@yahoo.com>
2025-11-05 12:38:13 -0800

    Added validation and Offboarding functions. Added Named Arguments

--- ps/azure_controller.ps1
+++ ps/azure_controller.ps1
@@ -1,5 +1,9 @@
 param(
-    [string]$UserId
+    [string]$UserId,
+    [bool]$OffboardAzureUser=$false,
+    [bool]$GetAzureUserGroups=$false,
+    [bool]$RemoveAzureUserGroups=$false,
+    [bool]$DisableAzureUserAccount=$false
 )
 
 $azureConfig = Get-Content -Path '.\config\azure_config.json' | ConvertFrom-Json
@@ -10,34 +14,6 @@ $azureConfig = Get-Content -Path '.\config\azure_config.json' | ConvertFrom-Json
 # )
 
 # Import-Module Microsoft.Graph
-
-function Get-AzureUserGroups{
-    param(
-        [string]$UserId
-    )
-    $userGroups = Get-MgUserMemberOf -UserId $UserId
-    return $userGroups
-}
-
-function Remove-AzureUserGroups{
-    param(
-        [string]$UserId,
-        [array]$Groups
-    )
-
-    $userObjectId = (Get-MgUser -UserId $UserId).Id
-
-    foreach ($group in $Groups){
-        $groupId = $group.Id
-        $groupData = Get-MgGroup -GroupId $groupId
-        $groupName = $groupData.DisplayName
-        #TODO try/catch
-        #TODO: Write to log
-        # Remove-MgGroupMemberByRef -GroupId $groupId -DirectoryObjectId $userObjectId
-    }
-
-}
-
 
 # $scopes = @(
 #     "Chat.ReadWrite.All"
@@ -55,33 +31,91 @@ function Remove-AzureUserGroups{
 
 Connect-MgGraph -TenantId $azureConfig.tenantId -NoWelcome
 
+function Get-AzureUserGroups{
+    param(
+        [string]$UserId
+    )
+    #TODO: handle if user does not exist
+    $userGroups = Get-MgUserMemberOf -UserId $UserId
+    return $userGroups
+}
+
+function Remove-AzureUserGroups{
+    param(
+        [string]$UserId,
+        [array]$Groups
+    )
+
+    $userObjectId = (Get-MgUser -UserId $UserId).Id
+
+    foreach ($group in $Groups){
+        $groupId = $group.Id
+        $groupData = Get-MgGroup -GroupId $groupId
+        $groupName = $groupData.DisplayName
+        #Distribution groups or mail enabled sec groups cannot be removed with Graph API
+        try{
+            $recipientTypeDetails = (Get-Recipient -Identity $groupId).RecipientTypeDetails 
+            if ($recipientTypeDetails -eq "GroupMailbox"){
+                #Microsoft 365
+                Remove-MgGroupMemberByRef -GroupId $groupId -DirectoryObjectId $userObjectId
+            }else{
+                #Distribution List
+                Remove-DistributionGroupMember -Identity $groupId -Member $UserId -Confirm:$false
+            }
+        }
+            #TODO: Write to log success user $UserId removed from $groupName
+        catch{
+            #TODO: Write to log failure 
+        }    
+    }
+
+}
+
 function Disable-AzureUserAccount{
     param(
         [string]$UserId
     )
 
     $userLicenses = Get-MgUserLicenseDetail -UserId $UserId
-    $licenseSkuIds = @()
-    foreach ($license in $userLicenses) {
-        $sku = $license.SkuId
-        $licenseSkuIds += $sku
-    }
+    #remove licenses if there are any assigned
+    if ($userLicenses){
 
-    #try/cath
-    #will fail if user has no licenses
-    Set-MgUserLicense -UserId $UserId -RemoveLicenses $licenseSkuIds -AddLicenses @()
+        $licenseSkuIds = @()
+        foreach ($license in $userLicenses) {
+            $sku = $license.SkuId
+            $licenseSkuIds += $sku
+        }
+
+        try{
+            #Remove licenses
+            Set-MgUserLicense -UserId $UserId -RemoveLicenses $licenseSkuIds -AddLicenses @()
+            #TODO: Write to log success 
+        }
+        catch{
+            #TODO: Write to log failure 
+        }
+    }
+    
+    #Disable user
     $params = @{
-	    accountEnabled = $false
+        accountEnabled = $false
     }
     Update-MgUser -UserId $UserId -BodyParameter $params
-
+    #TODO: Write to log success 
 }
 
-#azure controller uses userId email address. Use SamAccountName+"@"+azureConfig.domain
-$azureUserGroups = Get-AzureUserGroups -UserId $UserId
-# Remove-AzureUserGroups -UserId $UserId -Groups $azureUserGroups
-# Disable-AzureUserAccount -UserId $UserId
+if ($OffboardAzureUser -eq $true -and $UserId -ne ""){
+    #azure controller uses userId email address. Use SamAccountName+"@"+azureConfig.domain
+    #Call all functions
+    #If user does not exixts. Raise exemption and exit
+    $azureUserGroups = Get-AzureUserGroups -UserId $UserId
+    #Write to log
+    if($azureUserGroups){
+        Remove-AzureUserGroups -UserId $UserId -Groups $azureUserGroups
+    }
+    Disable-AzureUserAccount -UserId $UserId
+}
 
-
+#TODO: Remove APplication access to accounts
 
 
